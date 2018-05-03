8430adfa7e3f081344353dec979116b1f3809b7a
john ojuolape <john.ojuolape@digital.justice.gov.uk>
2018-05-03 14:44:16 +0100

    added frontend and changed url

--- moj_default/urls.py
+++ moj_default/urls.py
@@ -17,5 +17,5 @@ from django.conf.urls import url
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='base.html'), name='demo'),
+    url(r'^$', TemplateView.as_view(template_name='moj_app.html')),
 ]
