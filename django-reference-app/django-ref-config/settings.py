a913962df307e7609b5e76682581ccbd86aa1bbd
Todd Tyree <tatyree@gmail.com>
2018-05-09 11:37:28 +0100

    Set SECRET_KEY with ENV rather than hardcode
    Self-explainatory.

--- django-reference-app/django-ref-config/settings.py
+++ django-reference-app/django-ref-config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd^2sg(rd9&3s$uj46-di0i_14bcy84x$lxsjt)d0ur_h!q*_!w'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
