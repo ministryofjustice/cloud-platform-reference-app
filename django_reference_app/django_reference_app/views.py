61dd5ddad61df58d1d9e562f0e8e3d960d84c0e3
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2018-05-11 15:12:33 +0000

    add the S3 test

--- django_reference_app/django_reference_app/views.py
+++ django_reference_app/django_reference_app/views.py
@@ -1,5 +1,8 @@
+import os, platform
 from django.shortcuts import render
 from django_reference_app.modelforms import tableForms
+import boto3
+import botocore
 
 
 def showform(request):
@@ -14,3 +17,25 @@ def showform(request):
         
     return render(request, 'clusterwebtwo.html', context)
 
+def s3test(request):
+  BUCKET_NAME = 'cloud-platform-reference-bucket'
+  FILE_NAME = 'hello.txt'
+  with open(FILE_NAME, 'rb') as f:
+    fstr = f.read()
+  if request.method == 'POST':
+    pass
+  else:
+    if (os.getppid() == 1):
+      osname = 'Docker'
+    else:
+      osname = platform.system()
+  status = ""
+  try:
+    session = boto3.Session()
+    s3_client = session.client('s3')
+    s3_client.put_object(Body=fstr, Bucket=BUCKET_NAME, Key='upload-'+FILE_NAME)
+    status = "Write OK"
+  except botocore.exceptions.ClientError as e:
+    status = "Write Failed"
+  return render(request, 'test-s3.html', {'osname':osname, 'status': status})
+  
