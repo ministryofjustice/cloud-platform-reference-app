3a5b469cfb556847d58ef4ebe462aac674b34f01
Alejandro Novotny <Alejandro.Novotny@digital.justice.gov.uk>
2018-05-18 11:16:37 +0100

    fixed identation, and newlines to comply with pep8, removed unwanted tests and helper method to run the test

--- django_reference_app/tests/test_homepage.py
+++ django_reference_app/tests/test_homepage.py
@@ -1,6 +1,8 @@
 from django import test
 
+
 class URLTests(test.TestCase):
-  def test_homepage(self):
-    response = self.client.get('/')
-    self.assertEqual(response.status_code, 200)
+
+    def test_homepage(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
--- django_reference_app/tests/test_model.py
+++ django_reference_app/tests/test_model.py
@@ -1,11 +1,9 @@
 from django import test
 from django_reference_app.models import Person
 
-class PersonTest(test.TestCase):
-  def create_person(self, name="test", job="only a test"):
-    return Person.objects.create(name=name, job=job)
 
-  def test_person_creation(self):
-    w = self.create_person()
-      self.assertTrue(isinstance(w, Person))
-      self.assertEqual(w.__str__(), w.name)
+class PersonTest(test.TestCase):
+
+    def test_person_creation(self):
+        w = Person.objects.create(name='test_user', job='test_job')
+        self.assertEqual(w.__str__(), w.name)
