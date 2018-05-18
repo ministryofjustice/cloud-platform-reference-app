6a70bb5ae1e81d4d000c02d90648cccb3204c563
Alejandro Novotny <Alejandro.Novotny@digital.justice.gov.uk>
2018-05-18 11:30:00 +0100

    changed variable name to a more descriptive one

--- django_reference_app/tests/test_model.py
+++ django_reference_app/tests/test_model.py
@@ -5,5 +5,5 @@ from django_reference_app.models import Person
 class PersonTest(test.TestCase):
 
     def test_person_creation(self):
-        w = Person.objects.create(name='test_user', job='test_job')
-        self.assertEqual(w.__str__(), w.name)
+        person = Person.objects.create(name='test_user', job='test_job')
+        self.assertEqual(person.__str__(), person.name)
