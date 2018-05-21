bb8d6f515d8b25874f678c31d2af824a5203e6fd
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2018-05-21 17:21:03 +0100

    Delete commented-out cruft

--- guestbook/views.py
+++ guestbook/views.py
@@ -11,7 +11,7 @@ def guestlist(request):
     if form.is_valid():
         form.save()
         redirect('guestbook:guestlist')
-    # name = request.GET['name']
+    
     name = request.POST['name']
     job = request.POST['job']
 
